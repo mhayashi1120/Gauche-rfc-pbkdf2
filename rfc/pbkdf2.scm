09201621dc71ecab024b45114a60196991de380c
Masahiro Hayashi <mhayashi1120@gmail.com>
2023-02-15 18:32:15 +0900

    import initial script from local

--- rfc/pbkdf2.scm
+++ rfc/pbkdf2.scm
@@ -1,42 +1,160 @@
 ;;;
-;;; gauche_rfc_pbkdf2
+;;; rfc.pbkdf2
 ;;;
 
 (define-module rfc.pbkdf2
+  (use util.digest)
   (use util.match)
   (use gauche.uvector)
   (export
-   compute-pbkdf2))
+   generate-hmac
+   compute-pbkdf2 compute-pbkdf2-hmac))
 (select-module rfc.pbkdf2)
+
+;; <u8vector> -> <u8vector> -> <u8vector>
+(define (u8xor u1 u2)
+  (unless (= (u8vector-length u1) (u8vector-length u2))
+    (error "Not a same size" u1 u2))
+  (u8vector-map (cut logxor <> <>) u1 u2))
+
+;; - SIZE : <integer>
+;; - VALUE : <integer>
+;; -> <u8vector>
+(define (pack-octet size value)
+  (let loop ([v value]
+             [i size]
+             [r ()])
+    (cond
+     [(= 0 i)
+      (apply u8vector r)]
+     [else
+      (loop (ash v -8)
+            (- i 1)
+            (cons (logand #xff v) r))])))
+
+(define (compute-size hasher)
+  ;; Get metaclass value
+  (slot-ref hasher 'hmac-block-size))
+
+(define (check-positive i name)
+  (unless (positive? i)
+    (errorf "Argument `~a` must be a positive number (But ~a)" name i)))
+
+(define (with-input-from-bytes b thunk)
+  (let1 op (open-input-bytevector b)
+    (unwind-protect
+     (with-input-from-port op thunk)
+     (close-port op))))
+
+(autoload rfc.md5 <md5>)
+(autoload rfc.sha1 <sha1> <sha224> <sha256> <sha384> <sha512>)
+(autoload rfc.hmac hmac-digest)
+(autoload gauche.vport open-input-bytevector open-output-bytevector get-output-bytevector)
+
+;; -> <message-digest-algorithm>
+(define (ensure-hasher hasher)
+  (match hasher
+    ['sha256 <sha256>]
+    ['sha512 <sha512>]
+    ['sha224 <sha224>]
+    ['sha384 <sha384>]
+    ['sha1 <sha1>]
+    ['md5 <md5>]
+    [(? (^x (subclass? x <message-digest-algorithm>)) clz)
+     clz]
+    [else
+     (error "Not a supported hasher" hasher)]))
+
+;; -> <integer>
+(define (compute-block-size prf)
+  (u8vector-length (prf "" #u8())))
 
 ;;;
 ;;; # API
 ;;;
 
-;; ##
-;; - PASSWORD : <string> | <u8vector> TODO
-;; - ITER : <integer> Count of iteration
-;; - LEN : <integer>
-;; - :PRF : (PASSWORD:<u8vector> INPUT:<u8vector>) -> <u8vector>
+;; ## Basic:
+;;
+;; <PRF>     ::= (PASSWORD:<string> INPUT:<u8vector>) -> <u8vector>
+
+;; TODO consider rename prnd -> prf
+
+;; ## Low level API
+;; - PASSWORD : <string> | <u8vector>
+;; - ITER : <integer> Count of iteration.
+;; - LEN : <integer> Request length of  result.
+;; - :prnd : <PRF>
+;; - :block-size : <integer> To suppress the overhead of the compution,
+;;           should assign this value which is length of `prnd` generated.
 ;; - :salt : <u8vector>
+;; -> <u8vector>[LEN]
+(define (compute-pbkdf2
+         password iter len
+         :key (prnd (generate-hmac <sha256>))
+         (block-size (compute-block-size prnd))
+         (salt #u8()))
+
+  ;; <string>
+  (define P (match password
+              [(? string?)
+               password]
+              [(? u8vector?)
+               (u8vector->string password)]
+              [else
+               (error "Not a supported password")]))
+
+  ;; -> <u8vector>
+  (define (F* U :optional (i 1))
+    (let1 Ux (prnd P U)
+      (cond
+       [(= i iter) Ux]
+       [else
+        (u8xor Ux (F* Ux (+ i 1)))])))
+
+  ;; -> <list BYTE:<integer>>
+  (define (DK request-size :optional (i 1))
+    (let* ([U0 (u8vector-append salt (pack-octet 4 i))]
+           [T (u8vector->list (F* U0))]
+           [size* (- request-size (length T))])
+      (cond
+       [(<= size* 0)
+        (take T request-size)]
+       [else
+        (append T (DK size* (+ i 1)))])))
+
+  (assume-type iter <integer>)
+  (assume-type len <integer>)
+  (assume-type block-size <integer>)
+  (and salt (assume-type salt <u8vector>))
+  
+  (check-positive iter "iter")
+  (check-positive len "len")
+  (check-positive block-size "block-size")
+  
+  (when (< (* #xffffffff block-size) len)
+    (error "Invalid length of request" len))
+
+  ($ list->u8vector $ DK len))
+
+;; ## High level api of `compute-pbkdf2`
+;; - :hasher : {sha256 | sha512 | sha224 | sha384 | sha1 | md5}<symbol> | <digest-class>
 ;; -> <u8vector>
-(define (compute-pbkdf2 password iter len :key (PRF #f) (salt #f))
-  (let ([pass (match password
-                [(? string?)
-                 (string->u8vector password)]
-                [(? u8vector?)
-                 password]
-                [else
-                 (error "Not a supported password")])])
-    ;; TODO #xffffffff
-    ))
+(define (compute-pbkdf2-hmac
+         password iter len
+         :key (hasher 'sha256) (salt #u8()))
+  (let1 hasher* (ensure-hasher hasher)
+    (compute-pbkdf2 password iter len
+                    :prnd (generate-hmac hasher*)
+                    :block-size (~ hasher*'hmac-block-size)
+                    :salt salt)))
 
-
-(autoload rfc.hmac hmac-digest)
-
-;; ##
-;; -> TODO -> <u8vector>
+;; ## Generate HMAC procedure.
+;; - HASHER : <message-digest-algorithm>
+;; -> <PRF>
 (define (generate-hmac hasher)
   (^ [pass input]
-    ;; TODO
-    (hmac-digest :hasher hasher)))
+    (with-input-from-bytes
+     input
+     (^[]
+       ($ string->u8vector
+          $ hmac-digest :key pass :hasher hasher)))))
--- test.scm
+++ test.scm
@@ -2,10 +2,186 @@
 ;;; Test gauche_rfc_pbkdf2
 ;;;
 
+(use compat.chibi-test)
 (use gauche.test)
 
 (test-start "rfc.pbkdf2")
+
 (use rfc.pbkdf2)
 (test-module 'rfc.pbkdf2)
+
+(debug-print-width #f)
+
+(use gauche.uvector)
+(use rfc.sha1)
+(use gauche.process)
+
+(define (->hex u8)
+  (with-output-to-string
+    (^[]
+      (u8vector-map (^b (format #t "~2,'0X" b) b) u8))))
+
+(define (hex->u8vector h)
+  (let1 hex (regexp-replace-all* h #/[ \n]/ "")
+    (with-input-from-string hex
+      (^[]
+        (let loop ([l ()])
+          (let1 b (read-string 2)
+            (cond
+             [(eof-object? b)
+              (list->u8vector (reverse! l))]
+             [else
+              (loop (cons (string->number b 16) l))])))))))
+
+(define (pbkdf2-sha1 pass iter size salt)
+  (compute-pbkdf2-hmac pass iter size :salt salt :hasher 'sha1))
+
+;; -> [KEY:<string> IV:{<string> | #f}]
+(define (openssl-key&iv pass iter size :key (salt #f) (hash-algorithm 'sha256))
+  (and-let* ([args (cond-list
+                    [salt @ (list  "-S" salt)]
+                    [(not salt) @ (list "-nosalt")]
+                    [#t (ecase size
+                          [(16)
+                           "-aes-128-ctr"]
+                          [(24)
+                           "-aes-192-ctr"]
+                          [(32)
+                           "-aes-256-ctr"])]
+                    )]
+             [out (process-output->string
+                   `(openssl enc
+                             -md ,hash-algorithm
+                             -e -pbkdf2
+                             -iter ,iter
+                             -pass ,(format "pass:~a" pass)
+                             -P ,@args))]
+             [m-key (#/key *=([0-9a-f]+)/i out)]
+             [key (m-key 1)])
+    (if-let1 m-iv (#/iv *=([0-9a-f]+)/i out)
+      (values key (m-iv 1))
+      (values key #f))))
+
+(define (openssl-key . args)
+  (values-ref (apply openssl-key&iv args) 0))
+
+
+;; (dolist (iter '(100000))
+;;   (let ([pass "hogehoge1"]
+;;         [size 32]
+;;         [algo 'md5])
+;;     #?= (equal? (openssl-key pass iter size :hash-algorithm algo)
+;;                 (->hex (compute-pbkdf2-hmac
+;;                         pass iter size
+;;                         :hasher algo)))))
+
+;; (exit 0)
+(chibi-test
+ (test-group "RFC 3962 (B.  Sample Test Vectors)"
+   (test-group "password with salt 1"
+     (test
+      (hex->u8vector "cd ed b5 28 1b b2 f8 01 56 5a 11 22 b2 56 35 15")
+      (pbkdf2-sha1 "password" 1 16  (string->u8vector "ATHENA.MIT.EDUraeburn")))
+     (test
+      (hex->u8vector "cd ed b5 28 1b b2 f8 01 56 5a 11 22 b2 56 35 15 0a d1 f7 a0 4b b9 f3 a3 33 ec c0 e2 e1 f7 08 37")
+      (pbkdf2-sha1 "password" 1 32  (string->u8vector "ATHENA.MIT.EDUraeburn"))))
+
+   (test-group "password with salt 2"
+     (test
+      (hex->u8vector "01 db ee 7f 4a 9e 24 3e 98 8b 62 c7 3c da 93 5d")
+      (pbkdf2-sha1 "password" 2 16  (string->u8vector "ATHENA.MIT.EDUraeburn")))
+     (test
+      (hex->u8vector "01 db ee 7f 4a 9e 24 3e 98 8b 62 c7 3c da 93 5d a0 53 78 b9 32 44 ec 8f 48 a9 9e 61 ad 79 9d 86")
+      (pbkdf2-sha1 "password" 2 32  (string->u8vector "ATHENA.MIT.EDUraeburn"))))
+
+   (test-group "password with salt 3"
+     (test
+      (hex->u8vector "5c 08 eb 61 fd f7 1e 4e 4e c3 cf 6b a1 f5 51 2b")
+      (pbkdf2-sha1 "password" 1200 16  (string->u8vector "ATHENA.MIT.EDUraeburn")))
+     (test
+      (hex->u8vector "5c 08 eb 61 fd f7 1e 4e 4e c3 cf 6b a1 f5 51 2b a7 e5 2d db c5 e5 14 2f 70 8a 31 e2 e6 2b 1e 13")
+      (pbkdf2-sha1 "password" 1200 32  (string->u8vector "ATHENA.MIT.EDUraeburn"))))
+
+   (test-group "password with salt 4"
+     (test
+      (hex->u8vector "d1 da a7 86 15 f2 87 e6 a1 c8 b1 20 d7 06 2a 49")
+      (pbkdf2-sha1 "password" 5 16  (hex->u8vector "1234567878563412")))
+     (test
+      (hex->u8vector "d1 da a7 86 15 f2 87 e6 a1 c8 b1 20 d7 06 2a 49 3f 98 d2 03 e6 be 49 a6 ad f4 fa 57 4b 6e 64 ee")
+      (pbkdf2-sha1 "password" 5 32  (hex->u8vector "1234567878563412"))))
+
+   (test-group "Pass phrase = (64 characters)"
+     (test
+      (hex->u8vector "13 9c 30 c0 96 6b c3 2b a5 5f db f2 12 53 0a c9")
+      (pbkdf2-sha1 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" 1200 16  (string->u8vector "pass phrase equals block size")))
+     (test
+      (hex->u8vector "13 9c 30 c0 96 6b c3 2b a5 5f db f2 12 53 0a c9 c5 ec 59 f1 a4 52 f5 cc 9a d9 40 fe a0 59 8e d1")
+      (pbkdf2-sha1 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" 1200 32  (string->u8vector "pass phrase equals block size"))))
+
+   (test-group "Pass phrase = (65 characters)"
+     (test
+      (hex->u8vector "9c ca d6 d4 68 77 0c d5 1b 10 e6 a6 87 21 be 61")
+      (pbkdf2-sha1 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" 1200 16 (string->u8vector "pass phrase exceeds block size")))
+     (test
+      (hex->u8vector "9c ca d6 d4 68 77 0c d5 1b 10 e6 a6 87 21 be 61 1a 8b 4d 28 26 01 db 3b 36 be 92 46 91 5e c8 2a")
+      (pbkdf2-sha1 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" 1200 32 (string->u8vector "pass phrase exceeds block size"))))
+                 
+   (test-log "Testing ~s" (with-output-to-string (^[] (write-bytevector #u8(#xf0 #x9d #x84 #x9e)))))
+
+   (test-group "Pass phrase = g-clef (0xf09d849e) (musical-symbol UTF-8 sequence)"
+     (test
+      (hex->u8vector "6b 9c f2 6d 45 45 5a 43 a5 b8 bb 27 6a 40 3b 39")
+      (pbkdf2-sha1 (hex->u8vector "f09d849e") 50 16 (string->u8vector "EXAMPLE.COMpianist")))
+     (test
+      (hex->u8vector "6b 9c f2 6d 45 45 5a 43 a5 b8 bb 27 6a 40 3b 39 e7 fe 37 a0 c4 1e 02 c2 81 ff 30 69 e1 e9 4f 52")
+      (pbkdf2-sha1 (hex->u8vector "f09d849e") 50 32 (string->u8vector "EXAMPLE.COMpianist"))))
+   )
+
+ (test-group "low-level compute-pbkdf2"
+   (let ([pass "password0001"])
+     (test
+      (openssl-key pass 1 32 :hash-algorithm 'sha256)
+      (->hex (compute-pbkdf2 pass 1 32)))))
+
+ (test-group "same as openssl command"
+   (dolist (pass '("password0001" "qwerty"))
+     (test-group (format "Password ~s" pass)
+       (dolist (size '(16 24 32))
+         (test-group (format "Size ~a" size)
+           (dolist (algo '(md5 sha256))
+             (test-group (format "Algorithm ~a" algo)
+               (dolist (iter '(1 2 3 4 5 100))
+                 (test (openssl-key pass iter size :hash-algorithm algo)
+                       (->hex (compute-pbkdf2-hmac
+                               pass iter size
+                               :hasher algo)))
+
+                 (test (openssl-key pass iter size :salt "0102030405060708" :hash-algorithm algo)
+                       (->hex (compute-pbkdf2-hmac
+                               pass iter size
+                               :hasher algo
+                               :salt (u8vector 1 2 3 4 5 6 7 8)))))))))))
+
+
+   (test-group "Large iterations"
+     ;; openssl aes-192-ctr -md sha256 -e -pbkdf2 -iter 10000 -pass pass:hogehoge3021 -P -nosalt
+     (test "EC863DA2A2EE13A85045053C647474A9DB884942794E629B"
+           (->hex (compute-pbkdf2-hmac
+                   "hogehoge3021" 10000 24
+                   :hasher <sha256>)))
+
+     ;; openssl aes-256-ctr -md sha256 -e -pbkdf2 -iter 10000 -pass pass:qwerty -P -S "0102030405060708"
+     (test "517FE483AF63FF8B51946F7B422BEB349F9C7CA7C9B4574914A21A8E44D94501"
+           (->hex (compute-pbkdf2-hmac
+                   "qwerty" 10000 32
+                   :hasher <sha256>
+                   :salt (u8vector 1 2 3 4 5 6 7 8))))
+
+     ;; openssl aes-256-ctr -md sha256 -e -pbkdf2 -iter 10000 -pass pass:hogehoge3021 -P -nosalt
+     (test "EC863DA2A2EE13A85045053C647474A9DB884942794E629BF3CA71C5C3F511F7"
+           (->hex (compute-pbkdf2-hmac
+                   "hogehoge3021" 10000 32
+                   :hasher <sha256>)))
+     )))
 
 (test-end :exit-on-failure #t)
