dc72f9cd1248b3bf189dbabf4434ff1ffc5d022a
Masahiro Hayashi <mhayashi1120@gmail.com>
2023-04-20 19:20:48 +0900

    fix project setting

--- _docker/run-check.sh
+++ _docker/run-check.sh
@@ -15,4 +15,4 @@ git diff . | patch --strip 1 --directory "${WORK_DIR}"
 
 cd "${WORK_DIR}"
 
-docker run -v ${WORK_DIR}:/home/app --rm -ti practicalscheme/gauche sh -c 'cd /home/app && ./_docker/setup.sh && ./run-ci.sh'
+docker run -v ${WORK_DIR}:/home/app --rm -ti practicalscheme/gauche sh -c 'cd /home/app && ./run-ci.sh'
